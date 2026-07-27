4946739da0db95f8450ce3ad5a6c76c59da9ada8
bogon0130 <loveses7@gmail.com>
2026-07-27 22:59:21 +0000

    init: tinadmin-desktop Tauri 앱 스캐폴딩 (vite+shadcn, GitHub Actions 릴리즈 워크플로우 포함)

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { ExternalLinkGuard } from "./components/external-link-guard.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <ExternalLinkGuard />
+      <main data-ui-scroll-container><App /></main>
     </ThemeProvider>
   </StrictMode>
 )
